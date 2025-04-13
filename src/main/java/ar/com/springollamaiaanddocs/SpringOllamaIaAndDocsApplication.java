4d71700bb35f87626ed059dc4eb8e0ba3cd9681c
AlanAruFlores <alanaruquipa242@gmail.com>
2025-04-13 16:30:47 -0300

    spring project and ollama docker container implementation

---
+++ src/main/java/ar/com/springollamaiaanddocs/SpringOllamaIaAndDocsApplication.java
@@ -0,0 +1,13 @@
+package ar.com.springollamaiaanddocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOllamaIaAndDocsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringOllamaIaAndDocsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/com/springollamaiaanddocs/SpringOllamaIaAndDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.springollamaiaanddocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOllamaIaAndDocsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
